546c401c6e134169e86fd651d4c87cebcb66b3bb
razane <wakhidirazane@gmail.com>
2026-01-03 16:20:40 +0000

    j'ai crée la classe de database pour la connexion et now im preparing the userInterface to create the user classe

---
+++ config/database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE kari ;
+use kari;
+CREATE TABLE roles (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL UNIQUE
+);
+INSERT INTO roles(role_id,role_name) VALUES
+(1,"admin"),
+(2,"host"),
+(3,"traveler");
+
+-- Users table
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+
